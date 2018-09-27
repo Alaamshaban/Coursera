9f79f9a687a65d925113e20c706c3001d5c24c4f
alaa <alaa.mohamed@diracsystems.com>
2018-09-27 17:27:23 +0200

    dish-detail component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
