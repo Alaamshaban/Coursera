c699ae096a2e67602bdee2b8fa85214aa124d0c6
alaa <alaa.mohamed@diracsystems.com>
2018-09-30 18:01:24 +0200

    week 2

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +22,9 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
